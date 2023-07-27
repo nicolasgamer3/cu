33374ae839755200e5a66fb37896fdd594270038
nicolasgamer3 <140570496+nicolasgamer3@users.noreply.github.com>
2023-07-27 19:29:05 -0300

    Add files via upload

---
+++ minecraft spoofer.bat
@@ -0,0 +1,15 @@
+@echo off
+color 1E
+:gotAdmin
+echo vamo spoofar essa merda 
+
+ipconfig /release
+ipconfig /flushdns
+ipconfig /renew 
+
+echo spoofando .....
+
+echo spoofou! bom jogo Meu piru vai xitar fdp
+@echo 
+
+pause
